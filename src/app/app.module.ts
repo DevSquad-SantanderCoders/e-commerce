30616924af58095928a24a79fabd3889a56cf8df
Thayza de Oliveira <thayzaoliveira4@gmail.com>
2023-11-15 02:40:57 -0300

    chore: Add necessary imports for modals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { registerLocaleData } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import ptBr from '@angular/common/locales/pt';
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -6,6 +7,7 @@ import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatOptionModule } from '@angular/material/core';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
@@ -19,14 +21,14 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { DeleteProductFormComponent } from './modais/delete-product-form/delete-product-form.component';
+import { EditProductFormComponent } from './modais/edit-product-form/edit-product-form.component';
 import { AddComponent } from './views/add/add.component';
 import { HomeComponent } from './views/home/home.component';
-import { LoginComponent } from './views/login/login.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HttpClientModule } from '@angular/common/http';
 import { ListarProdutosComponent } from './views/listarProdutos/listar-produtos.component';
-
+import { LoginComponent } from './views/login/login.component';
 
 registerLocaleData(ptBr);
 
@@ -40,6 +42,8 @@ registerLocaleData(ptBr);
     HeaderComponent,
     FooterComponent,
     ListarProdutosComponent,
+    EditProductFormComponent,
+    DeleteProductFormComponent,
   ],
   imports: [
     BrowserModule,
@@ -59,7 +63,7 @@ registerLocaleData(ptBr);
     MatOptionModule,
     NgbModule,
     HttpClientModule,
-
+    MatDialogModule,
   ],
   providers: [{ provide: LOCALE_ID, useValue: 'pt' }],
   bootstrap: [AppComponent],
