2e9e68b7212217dcefee8e8337f203e53e91bedd
Thayza de Oliveira <thayzaoliveira4@gmail.com>
2023-11-15 02:36:09 -0300

    feat: Create modals to apply CRUD

---
+++ src/app/modais/edit-product-form/edit-product-form.component.ts
@@ -0,0 +1,28 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { IProduct } from 'src/app/models/product-data.model';
+
+@Component({
+  selector: 'app-edit-product-form',
+  templateUrl: './edit-product-form.component.html',
+  styleUrls: ['./edit-product-form.component.scss'],
+})
+export class EditProductFormComponent {
+  currentProduct!: IProduct;
+  editProduct: boolean = false;
+  constructor(
+    public dialogRef: MatDialogRef<EditProductFormComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    if (this.data) {
+      this.currentProduct = this.data;
+      this.editProduct = true;
+    } else {
+      this.editProduct = false;
+    }
+  }
+
+  closeModal() {
+
+  }
+}
--- src/app/views/add/add.component.ts
+++ src/app/views/add/add.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { EditProductFormComponent } from 'src/app/modais/edit-product-form/edit-product-form.component';
+import { IProduct } from 'src/app/models/product-data.model';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrls: ['./add.component.scss']
+  styleUrls: ['./add.component.scss'],
 })
 export class AddComponent {
+  @Input() editProduct: boolean = false;
+  @Input() currentProduct!: IProduct;
+  // @Output() public closeModal = new EventEmitter<any>();
+  constructor(public dialogRef: MatDialogRef<EditProductFormComponent>) {}
+  closeModal() {
+    this.dialogRef.close();
+  }
 
+  ngOnInit() {}
 }
