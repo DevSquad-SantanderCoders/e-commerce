4cb64795365d4cdb1f2c2275d459e84f7c989f44
Juan Salles <juanprazeres@gmail.com>
2023-11-16 23:27:04 -0300

    feat: Update the Login feature

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent {
+  usuario: string = ""
+  senha: string = ""
+
+  constructor(private authService: AuthService, private router: Router){
+
+  }
+
+  login(){
+    this.authService.login(this.usuario, this.senha).subscribe((result)=>{
+      if (result){
+        this.authService.saveLogin();
+        this.router.navigate(['/']);
+      }else{
+        console.log("Usuario não existe");
+        this.router.navigate(['/login']);
+      }
+    });
+  }
 
 }
